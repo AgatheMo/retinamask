5d5c11220040d8e345e7812e85be5629dcf89153
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2021-02-16 22:13:10 -0500

    Update inference.py

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -88,7 +88,7 @@ def prepare_for_coco_segmentation(predictions, dataset):
         prediction = prediction.resize((image_width, image_height))
         masks = prediction.get_field("mask")
         # t = time.time()
-        masks = self.masker([masks], [prediction])[0]
+        masks = Masker([masks], [prediction])[0]
         # logger.info('Time mask: {}'.format(time.time() - t))
         # prediction = prediction.convert('xywh')
 
